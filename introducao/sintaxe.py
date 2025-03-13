cf75938f7c4bd959f03d66cc7adba9c2f9f9053b
Erick-A-Martins <erickalvesqmartins@gmail.com>
2025-03-13 16:01:50 -0300

    Initial commit

---
+++ introducao/sintaxe.py
@@ -0,0 +1,7 @@
+# Esse é meu primeiro comentário
+"""
+    Comentário em múltiplas linhas
+    Segunda linha de comentário
+"""
+
+nome = "Erick" + " Alves"
---
+++ introducao/variaveis.py
@@ -0,0 +1,4 @@
+nome_completo = "Erick Alves de Queiroz Martins"
+idade = 25
+
+# camelCase para classes / snake_case para viriáveis, funções e métodos
